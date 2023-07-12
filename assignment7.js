d8882be7220667920ef1900d4c4315b1617e7f55
okantas <okantas954@gmail.com>
2023-07-12 15:40:22 +0300

    last changes

--- assignment7.js
+++ assignment7.js
@@ -3,9 +3,13 @@
 function addDigits(number){
  
     let totalOfDigits = 0;
+
+    //turn the number as a string form
     let stringOfNumber = number.toString()
 
     for(let i = 0 ; i < stringOfNumber.length ; i++ ){
+
+        //charAt give the oppotunity to select characters one by one
 
         totalOfDigits += parseInt(stringOfNumber.charAt(i)) 
 
--- assignment8.js
+++ assignment8.js
@@ -4,10 +4,12 @@ function evenOrOdd(number){
     if(number%2 == 0){
 
         console.log(`${number} is even`)
+
     }
     else{
 
         console.log(`${number} is odd`)
+
     }
 
 }
